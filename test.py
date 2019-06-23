1151b22d7d4d69fc7888a3f1f9cca5080c1812b9
Yuan Yao <yaoyuan0553@yahoo.com>
2019-06-23 22:20:21 +0800

    add another package to test on

--- test.py
+++ test.py
@@ -2,18 +2,18 @@ import torch
 import math
 from torch.optim import Adam
 from pytorch_pretrained_bert import BertTokenizer, BertConfig, BertEmbeddings
-from pytorch_pretrained_bert import BertPreTrainedModel
+from pytorch_pretrained_bert import BertPreTrainedModel, BertModel
 from pytorch_pretrained_bert import BertForTokenClassification, BertAdam
 from tqdm import tqdm
 from typing import List
 
 
 def cosineSimilarity(v1, v2):
-    dotProduct = sum(p*q for p,q in zip(v1, v2))
+    dotProduct = sum(p * q for p,q in zip(v1, v2))
     magnitude = math.sqrt(sum([val**2 for val in v1])) * math.sqrt(sum([val**2 for val in v2]))
     if not magnitude:
         return 0
-    return dotProduct/magnitude
+    return dotProduct / magnitude
 
 
 origWordSents = [
@@ -46,31 +46,60 @@ class BertPreTrainedEmbeddings(BertPreTrainedModel):
         return self.embeddings(paddedInputIds)
 
 
+def customEmbeddingTest():
+    tokenizer = BertTokenizer.from_pretrained('/media/yuan/Samsung_T5/Documents/BERT/bert-base-chinese')
+    bertEmbedModel = BertPreTrainedEmbeddings.from_pretrained('/media/yuan/Samsung_T5/Documents/BERT/bert-base-chinese')
+    bertModel = BertModel.from_pretrained('/media/yuan/Samsung_T5/Documents/BERT/bert-base-chinese')
+
+    sortedWordSents = sorted(origWordSents, key=lambda e: len(e), reverse=True)
+    print(sortedWordSents)
+
+
+    tokenizedSents = [tokenizer.tokenize(sent) for sent in sortedWordSents]
+    targetedWordIdxs = [sent.index('还') for sent in tokenizedSents]
+    wordIdxs = [tokenizer.convert_tokens_to_ids(sent) for sent in tokenizedSents]
+
+    maxLen = len(wordIdxs[0])
+    paddedInputIds = [sent + [0] * (maxLen - len(sent)) for sent in wordIdxs]
+    paddedInputIds = torch.tensor(paddedInputIds)
+
+    attentionMask = torch.tensor([[float(i > 0) for i in sent] for sent in paddedInputIds])
+
+    sentsEmbeds, _ = bertModel(paddedInputIds, attention_mask=None,
+                            output_all_encoded_layers=False)
+    print(targetedWordIdxs)
+    # print([wordIdxs[i][j] for i, j in enumerate(targetedWordIdxs)])
+    print(sortedWordSents[0])
+    print(tokenizedSents[0])
+    # print(sentsEmbeds[-1][targetedWordIdxs[-1]])
+    # print(sentsEmbeds[-3][targetedWordIdxs[-3]])
+
+    v1 = -3
+    v2 = -1
+    print(sortedWordSents[v1])
+    print(sortedWordSents[v2])
+    print(cosineSimilarity(
+        sentsEmbeds[v1][targetedWordIdxs[v1]],
+        sentsEmbeds[v2][targetedWordIdxs[v2]]))
+
+from flair.embeddings import BertEmbeddings as FlBertEmbeddings
+from flair.data import Sentence
+
+embedding = FlBertEmbeddings('/media/yuan/Samsung_T5/Documents/BERT/bert-base-chinese')
+
+sents = [Sentence(s) for s in origWordSents]
+print(sents)
+
 tokenizer = BertTokenizer.from_pretrained('/media/yuan/Samsung_T5/Documents/BERT/bert-base-chinese')
-model = BertPreTrainedEmbeddings.from_pretrained('/media/yuan/Samsung_T5/Documents/BERT/bert-base-chinese')
 
-sortedWordSents = sorted(origWordSents, key=lambda e: len(e), reverse=True)
-print(sortedWordSents)
+testSent = tokenizer.tokenize('劳动力还不够，必须再调一部分人过来。')
+print(testSent)
 
-tokenizedSents = [tokenizer.tokenize(sent) for sent in sortedWordSents]
-targetedWordIdxs = [sent.find('还') for sent in sortedWordSents]
-wordIdxs = [tokenizer.convert_tokens_to_ids(sent) for sent in tokenizedSents]
-sentsEmbeds = model(wordIdxs)
-print(targetedWordIdxs)
-# print([wordIdxs[i][j] for i, j in enumerate(targetedWordIdxs)])
-print(sortedWordSents[0])
-print(tokenizedSents[0])
-# print(sentsEmbeds[-1][targetedWordIdxs[-1]])
-# print(sentsEmbeds[-3][targetedWordIdxs[-3]])
+s = Sentence()
+for w in testSent:
+    s.add_token(w)
+embeddedSents = embedding.embed(s)
 
-v1 = 2
-v2 = -2
-print(sortedWordSents[v1])
-print(sortedWordSents[v2])
-print(cosineSimilarity(
-    sentsEmbeds[v1][targetedWordIdxs[v1]],
-    sentsEmbeds[v2][targetedWordIdxs[v2]]))
-# print(tokenized_texts[0])
 
 
 # if __name__ == "__main__":
